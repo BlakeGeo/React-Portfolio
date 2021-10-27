1846a6331e8a4498b952ae46d07ede4cf0e92056
Blake Georgeff <georgeffb@hotmail.com>
2021-10-27 18:13:58 +0800

    finished projects page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,29 +1,44 @@
 import React from "react";
 import noviceChef from './images/noviceChef.png';
 import book from './images/book-inventory.png';
+import tech from './images/tech-blog.png';
+import weather from './images/weatherDashboard.png';
+import github from './images/githubRed.png';
+import budget from './images/budgetTracker.png';
 
 
 export default function Portfolio() {
     return (
         <section class="project">
+			<h1>projects.</h1>
 			<div class="cards">
 				<div class="card">
 					<a href="https://github.com/BlakeGeo">
 						<img src="https://www.simplilearn.com/ice9/free_resources_article_thumb/responsibilities-full-stack.jpg" alt="project 3" class="card-image" />
+					</a>
 						<div class="card-info">
 							<h2>PROJECT 3</h2>
-							<p>React | Mongo | GraphQL</p>
+							<div class='tech-repo'>
+								<span class='techs'>React | Mongo | GraphQL</span>
+								<a href="https://github.com/BlakeGeo">
+									<img src={github} alt='github repo' class='gitRepo'/>
+								</a>
+							</div>
 						</div>
-					</a>
 				</div>
 				<div class="card">
-					<a href="https://github.com/BlakeGeo">
+					<a href="https://book-inventoryyy.herokuapp.com/">
 						<img src={book} alt="project 2-book inventory" class="card-image" />
+					</a>
 						<div class="card-info">
 							<h2>Book Inventory</h2>
-							<p>JavaScript | Handlebars | SQL</p>
+							<div class='tech-repo'>
+								<span class='techs'>JavaScript | Handlebars | SQL</span>
+								<a href="https://github.com/BlakeGeo/Book-Inventory-1">
+									<img src={github} alt='github repo' class='gitRepo'/>
+								</a>
+							</div>
 						</div>
-					</a>
 				</div>
 				<div class="card">
 					<a href="https://bt134.github.io/novice-chef/">
@@ -31,42 +46,56 @@ export default function Portfolio() {
 					</a>
 						<div class="card-info">
 							<h2>Novice Chef</h2>
-							<a href="https://github.com/BlakeGeo/novice-chef">
-								<p>Javascript | API</p>
-							</a>
+							<div class='tech-repo'>
+								<span class='techs'>Javascript | API</span>
+								<a href="https://github.com/BlakeGeo/novice-chef">
+									<img src={github} alt='github repo' class='gitRepo'/>
+								</a>
+							</div>
 						</div>
 				</div>
 				<div class="card">
 					<a href="https://techy-bloggg.herokuapp.com/">
-						<img src="./Images/tech-blog.png" alt="Tech-Blog" class="card-image" />
+						<img src={tech} alt="Tech-Blog" class="card-image" />
 					</a>
 						<div class="card-info">
 							<h2>Tech Blog</h2>
-							<a href="https://github.com/BlakeGeo/Tech-Blog">
-								<p>Node | SQL | Handlebars</p>
-							</a>
+							<div class='tech-repo'>
+								<span class='techs'>Node | SQL | Handlebars</span>
+								<a href="https://github.com/BlakeGeo/Tech-Blog">
+									<img src={github} alt='github repo' class='gitRepo'/>
+								</a>
+							</div>
 						</div>
 				</div>			
 				<div class="card">
+					<a href="https://immense-garden-00478.herokuapp.com/">
+						<img src={budget} alt="Budget-tracker" class="card-image" />
+					</a>
+						<div class="card-info">
+							<h2>Budget Tracker</h2>
+							<div class='tech-repo'>
+								<span class='techs'>Javascript | Mongo</span>
+								<a href="https://github.com/BlakeGeo/Budget-Tracker">
+									<img src={github} alt='github repo' class='gitRepo'/>
+								</a>
+							</div>
+						</div>
+				</div>
+				<div class="card">
 					<a href="https://blakegeo.github.io/Weather-Dashboard/">
-						<img src="./Images/weatherDashboard.png" alt="Weather-Dashboard" class="card-image" />
+						<img src={weather} alt="Weather-Dashboard" class="card-image" />
 					</a>
 						<div class="card-info">
 							<h2>Weather Dashboard</h2>
-							<a href="https://github.com/BlakeGeo/Weather-Dashboard">
-								<p>Javascript</p>
-							</a>
+							<div class='tech-repo'>
+								<span class='techs'>Javascript | API</span>
+								<a href="https://github.com/BlakeGeo/Weather-Dashboard">
+									<img src={github} alt='github repo' class='gitRepo'/>
+								</a>
+							</div>
 						</div>
-				</div>							
-				<div class="card">
-					<a href="https://github.com/BlakeGeo">
-						<img src="https://www.simplilearn.com/ice9/free_resources_article_thumb/responsibilities-full-stack.jpg" alt="project 2" class="card-image" />
-						<div class="card-info">
-							<h2>LED Wall</h2>
-							<p>Node</p>
-						</div>
-					</a>
-				</div>			
+				</div>														
 			</div>
 
 		</section>
