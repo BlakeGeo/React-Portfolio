c242491186d3267672708d9f1e27d03a44ef4ef1
Blake Georgeff <georgeffb@hotmail.com>
2021-10-27 16:50:07 +0800

    done about me page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,6 @@
 import React from "react";
+import noviceChef from './images/noviceChef.png';
+import book from './images/book-inventory.png';
 
 
 export default function Portfolio() {
@@ -6,13 +8,31 @@ export default function Portfolio() {
         <section class="project">
 			<div class="cards">
 				<div class="card">
+					<a href="https://github.com/BlakeGeo">
+						<img src="https://www.simplilearn.com/ice9/free_resources_article_thumb/responsibilities-full-stack.jpg" alt="project 3" class="card-image" />
+						<div class="card-info">
+							<h2>PROJECT 3</h2>
+							<p>React | Mongo | GraphQL</p>
+						</div>
+					</a>
+				</div>
+				<div class="card">
+					<a href="https://github.com/BlakeGeo">
+						<img src={book} alt="project 2-book inventory" class="card-image" />
+						<div class="card-info">
+							<h2>Book Inventory</h2>
+							<p>JavaScript | Handlebars | SQL</p>
+						</div>
+					</a>
+				</div>
+				<div class="card">
 					<a href="https://bt134.github.io/novice-chef/">
-						<img src="./Images/noviceChef.png" alt="Novice Chef project" class="card-image" />
+						<img src={noviceChef} alt="Novice Chef project 1" class="card-image" />
 					</a>
 						<div class="card-info">
 							<h2>Novice Chef</h2>
 							<a href="https://github.com/BlakeGeo/novice-chef">
-								<p>Javascript, API (repo link)</p>
+								<p>Javascript | API</p>
 							</a>
 						</div>
 				</div>
@@ -23,7 +43,7 @@ export default function Portfolio() {
 						<div class="card-info">
 							<h2>Tech Blog</h2>
 							<a href="https://github.com/BlakeGeo/Tech-Blog">
-								<p>Node, SQL, Handlebars (repo link)</p>
+								<p>Node | SQL | Handlebars</p>
 							</a>
 						</div>
 				</div>			
@@ -34,28 +54,10 @@ export default function Portfolio() {
 						<div class="card-info">
 							<h2>Weather Dashboard</h2>
 							<a href="https://github.com/BlakeGeo/Weather-Dashboard">
-								<p>Javascript (repo link)</p>
+								<p>Javascript</p>
 							</a>
 						</div>
-				</div>	
-				<div class="card">
-					<a href="https://github.com/BlakeGeo">
-						<img src="https://www.simplilearn.com/ice9/free_resources_article_thumb/responsibilities-full-stack.jpg" alt="project 2" class="card-image" />
-						<div class="card-info">
-							<h2>LED Wall</h2>
-							<p>Node</p>
-						</div>
-					</a>
-				</div>			
-				<div class="card">
-					<a href="https://github.com/BlakeGeo">
-						<img src="https://www.simplilearn.com/ice9/free_resources_article_thumb/responsibilities-full-stack.jpg" alt="project 2" class="card-image" />
-						<div class="card-info">
-							<h2>LED Wall</h2>
-							<p>Node</p>
-						</div>
-					</a>
-				</div>			
+				</div>							
 				<div class="card">
 					<a href="https://github.com/BlakeGeo">
 						<img src="https://www.simplilearn.com/ice9/free_resources_article_thumb/responsibilities-full-stack.jpg" alt="project 2" class="card-image" />
