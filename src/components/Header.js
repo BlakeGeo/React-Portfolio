a728ca707ffa7473245ce03266b259016525e714
Blake Georgeff <georgeffb@hotmail.com>
2021-10-28 17:53:51 +0800

    fixed JS loading issue

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import $ from 'jquery';
 
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector("#navBar");
-const navLink = document.querySelectorAll("#link-navbar");
-
-hamburger.addEventListener("click", mobileMenu);
-navLink.forEach(n => n.addEventListener("click", closeMenu));
-
-function mobileMenu() {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-}
-
-function closeMenu() {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-}
+$(document).ready(function() {
+  const hamburger = document.querySelector(".hamburger");
+  const navMenu = document.querySelector("#navBar");
+  const navLink = document.querySelectorAll("#link-navbar");
+  
+  hamburger.addEventListener("click", mobileMenu);
+  navLink.forEach(n => n.addEventListener("click", closeMenu));
+  
+  function mobileMenu() {
+      hamburger.classList.toggle("active");
+      navMenu.classList.toggle("active");
+  }
+  
+  function closeMenu() {
+      hamburger.classList.remove("active");
+      navMenu.classList.remove("active");
+  }
+  
+})
 
 const Header = () => (
   <header>
