1028b21cb9b33cb1e22e8f65bbd5a36fa72c312e
Blake Georgeff <georgeffb@hotmail.com>
2021-11-01 14:19:19 +0800

    finishing touches

--- src/components/main.js
+++ src/components/main.js
@@ -8,7 +8,7 @@ import { Switch, Route } from 'react-router-dom';
 
 const Main = () => (
   <Switch>
-    <Route exact path="/" component={Home} />
+    <Route exact path="/React-Portfolio" component={Home} />
     <Route path="/about" component={About} />
     <Route path="/projects" component={Projects} />
     <Route path="/curriculumvitae" component={CurriculumVitae} />
